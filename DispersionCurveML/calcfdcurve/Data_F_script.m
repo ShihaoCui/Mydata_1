c8ace408bb96122fdebbfb08c43fae815247bbed
ShihaoCui <50115572+ShihaoCui@users.noreply.github.com>
2022-10-31 18:10:12 -0400

    1
    1

--- DispersionCurveML/calcfdcurve/Data_F_script.m
+++ DispersionCurveML/calcfdcurve/Data_F_script.m
@@ -2,13 +2,13 @@ load('Data_F.mat')
 
 Njc = 1;
 for i=1:1:100000
-    if JSF(i)<0.07
-        DPRF_0_07(Njc,:) = DPRF(i,:);
-        VSF_0_07(Njc,:) = VSF(i,:);
+    if JSF(i)<0.06
+        DPRF_0_06(Njc,:) = DPRF(i,:);
+        VSF_0_06(Njc,:) = VSF(i,:);
         Njc = Njc+1;
     end
 end
 Njc
 min(JSF)
 
-save Data_F_0_07 DPRF_0_07 VSF_0_07
+save Data_F_0_06 DPRF_0_06 VSF_0_06
