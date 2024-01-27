01fbb2bf4ac6b4a59a89bc20a9b4a4a3836c9a09
ShihaoCui <50115572+ShihaoCui@users.noreply.github.com>
2024-01-27 17:06:33 -0500

    1
    1

--- XinxunLau3DPrintability/Data of result sharing/Model 2_PrintablityPredictor/DP.m
+++ XinxunLau3DPrintability/Data of result sharing/Model 2_PrintablityPredictor/DP.m
@@ -3,7 +3,7 @@ clc;
 close all;
 
 load('Data4.mat')
-scatter([1:1:48],Data4(:,1));
+% scatter([1:1:48],Data4(:,1));
 
 sd = Data4(:,1);
 
@@ -15,4 +15,24 @@ Data4_1 = Data4;
 Data4_1(:,1) = sd1;
 
 
+% size(find(sd1<0.50))
 
+Data4_2 = Data4_1;
+
+for i=1:1:48
+    if (Data4_1(i,1)>=0.95 )
+        Data4_2(i,1) = 0;
+    elseif (Data4_1(i,1)>=0.90 && Data4_1(i,1)<0.95)
+        Data4_2(i,1) = 1;
+    elseif (Data4_1(i,1)>=0.80 && Data4_1(i,1)<0.90)
+        Data4_2(i,1) = 2;
+    elseif (Data4_1(i,1)>=0.70 && Data4_1(i,1)<0.80)
+        Data4_2(i,1) = 3;
+    elseif (Data4_1(i,1)>=0.60 && Data4_1(i,1)<0.70)
+        Data4_2(i,1) = 4;
+    elseif (Data4_1(i,1)<0.60)
+        Data4_2(i,1) = 5;
+    end
+end
+
+save Data4_2 Data4_2 
