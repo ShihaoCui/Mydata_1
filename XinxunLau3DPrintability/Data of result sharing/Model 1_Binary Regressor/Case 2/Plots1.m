4f6caada450f39be8f474fbfc7c78a215cded2ae
ShihaoCui <50115572+ShihaoCui@users.noreply.github.com>
2024-01-27 15:58:59 -0500

    1
    1

---
+++ XinxunLau3DPrintability/Data of result sharing/Model 1_Binary Regressor/Case 2/Plots1.m
@@ -0,0 +1,26 @@
+clear all;
+clc;
+close all;
+
+load('y_train_predicted.mat')
+load('y_test_predicted.mat')
+load('y_test.mat')
+load('y_train.mat')
+load('loss.mat')
+
+figure
+y_test_predicted1=sign(y_test_predicted-0.5)==1;
+plot(y_test_predicted1)
+hold on 
+plot(y_test)
+
+figure
+y_train_predicted1=sign(y_train_predicted-0.5)==1;
+plot(y_train_predicted1)
+hold on 
+plot(y_train)
+
+figure
+plot(loss)
+
+
