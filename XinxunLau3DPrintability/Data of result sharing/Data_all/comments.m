f06d60818251b2bc6085884c761532499ed2f944
ShihaoCui <50115572+ShihaoCui@users.noreply.github.com>
2024-01-31 16:41:39 -0500

    1
    1

---
+++ XinxunLau3DPrintability/Data of result sharing/Data_all/comments.m
@@ -0,0 +1,4 @@
+% Data2: all data
+% Data3: for 0 1 prediction printable or not; Model 1
+% Data4_4: for printability prediction: MOdel 2. 
+% Data5: for 4 physical property estimation: Model 3
--- XinxunLau3DPrintability/Data of result sharing/Model 1_Binary Regressor/Case 2/Plots1.m
+++ XinxunLau3DPrintability/Data of result sharing/Model 1_Binary Regressor/Case 2/Plots1.m
@@ -10,17 +10,35 @@ load('loss.mat')
 
 figure
 y_test_predicted1=sign(y_test_predicted-0.5)==1;
+y_test_predicted1 = double(y_test_predicted1');
 plot(y_test_predicted1)
 hold on 
+y_test = y_test';
 plot(y_test)
 
 figure
 y_train_predicted1=sign(y_train_predicted-0.5)==1;
+y_train_predicted1 = double(y_train_predicted1);
 plot(y_train_predicted1)
 hold on 
+y_train = y_train';
 plot(y_train)
 
 figure
 plot(loss)
+for i=1:1:100
+    ID(i,1) = i*10;
+    LOSS(i,1) = loss(i*10);
+end
 
+figure
+C = confusionmat(y_train, y_train_predicted1);
+confusionchart(C);
 
+figure
+err_train = y_train-y_train_predicted1;
+plot(err_train)
+
+figure
+C1 = confusionmat(y_test,y_test_predicted1);
+confusionchart(C1);
