47bd3f32e245155290d618d166c78c2417041f85
ShihaoCui <50115572+ShihaoCui@users.noreply.github.com>
2024-03-06 04:01:49 -0500

    1
    1

--- HoumanMLVE/ML_for_Characterization/FiguresGenFun.m
+++ HoumanMLVE/ML_for_Characterization/FiguresGenFun.m
@@ -1,6 +1,6 @@
 function  pic= FiguresGenFun(u1, u2,rho, freq,v)
 
-pic = zeros(length(freq),length(v));
+pic = 0.01*ones(length(freq),length(v));
 Vs= DPRFun(u1, u2,rho, freq);
 for i=1:1:length(freq)
       for j=1:1:length(v)
---
+++ HoumanMLVE/ML_for_Characterization/dataProcess.m
@@ -0,0 +1,4 @@
+load('DataTrainViscoElatic.mat')
+pics = PicData(:,:,20);
+
+picjpg(:,:,1);
